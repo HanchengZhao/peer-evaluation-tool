d99517d2e48b640bc041864c501fae63e7ffc146
HanchengZhao <chancidy@gmail.com>
2016-06-26 19:08:19 +0000

    update

--- js/controllers.js
+++ js/controllers.js
@@ -1,42 +1,51 @@
+// CONTROLLERS
 app.controller("authCtrl", function($scope, $firebaseAuth) {
- $scope.authObj = $firebaseAuth();
- //login with google
+  $scope.authObj = $firebaseAuth();
+  //login with google
+  $scope.signIn = function() {
+      $scope.authObj.$signInWithPopup("google").then(function(firebaseUser) {
+        console.log("Signed in as:", firebaseUser.displayName);
+      }).catch(function(error) {
+        console.error("Authentication failed:", error);
+      });
+    }
+    
+    //sign-out
+  $scope.authObj.$signOut(function() {
 
- $scope.signIn = function() {
-     $scope.authObj.$signInWithPopup("google").then(function(firebaseUser) {
-       console.log("Signed in as:", firebaseUser.displayName);
-     }).catch(function(error) {
-       console.error("Authentication failed:", error);
-     });
-   }
-   //sign-out
- $scope.authObj.$signOut(function() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }, function(error) {
+      // An error happened.
+    });
+  })
 
-   firebase.auth().signOut().then(function() {
-     // Sign-out successful.
-   }, function(error) {
-     // An error happened.
-   });
- })
+  $scope.authObj.$onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+      $scope.firebaseUser = firebaseUser;
+      $scope.displayName = firebaseUser.displayName;
+      $scope.profilePicUrl = firebaseUser.photoURL || 'images/profile_placeholder.png';
+      $scope.email = firebaseUser.email;
+      $scope.userPic = $('#user-pic');
+      $scope.userPic.css('background-image', 'url(' + $scope.profilePicUrl + ')');
+      console.log("Signed in as:", firebaseUser.displayName);
 
- $scope.authObj.$onAuthStateChanged(function(firebaseUser) {
-   if (firebaseUser) {
-     $scope.firebaseUser = firebaseUser;
-     $scope.displayName = firebaseUser.displayName;
-     $scope.profilePicUrl = firebaseUser.photoURL || 'images/profile_placeholder.png';
-     $scope.email = firebaseUser.email;
-     $scope.userPic = $('#user-pic');
-     // console.log($scope.profilePicUrl);
-     // $("#user-pic").css('background-image','url(https://lh4.googleusercontent.com/-wnTI5FMslk8/AAAAAAAAAAI/AAAAAAAAAzk/xRUP7BTC8EE/s96-c/photo.jpg)');
-     $scope.userPic.css('background-image', 'url(' + $scope.profilePicUrl + ')');
-     console.log("Signed in as:", firebaseUser.displayName);
+    } else {
+      $scope.firebaseUser = false;
+      console.log("Signed out");
+      $scope.userPic.css('background-image', 'url(/images/profile_placeholder.png)');
 
-   } else {
-     $scope.firebaseUser = false;
-     console.log("Signed out");
-     $scope.userPic.css('background-image', 'url(/images/profile_placeholder.png)');
-
-   }
- });
+    }
+  });
 
 });
+
+
+
+app.controller('homeCtrl', ['$scope', '$location', function($scope, $location) {
+    
+}]);
+
+app.controller('studentCtrl', ['$scope', function($scope) {
+    
+}]);
