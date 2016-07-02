42aaa9e4ad0a50fe9a03817243b1f7a9f7727335
HanchengZhao <chancidy@gmail.com>
2016-07-02 08:54:07 +0000

    update

--- js/core/controllers.js
+++ js/core/controllers.js
@@ -255,6 +255,7 @@ app.controller("questionsGenerateCtrl", ["$scope", "$firebaseObject", "$firebase
    };
 }]);
 
+
 app.controller('studentCtrl', ['$scope', 'firebaseService', function($scope, firebaseService) {
     $scope.questionData = firebaseService.retrieveData("Questions-Data");
     console.log($scope.questionData);
