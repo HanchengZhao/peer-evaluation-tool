aca12718a2ade30303b5fa9698dc748c259649e8
HanchengZhao <chancidy@gmail.com>
2016-10-03 01:37:27 +0000

    update

--- js/core/config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
- apiKey: "AIzaSyAjHp1OIGV2kHqTq2J9eFa88GhtuuVDOpM",
- authDomain: "project-2114532762902987550.firebaseapp.com",
- databaseURL: "https://project-2114532762902987550.firebaseio.com",
- storageBucket: "",
-};
-
-firebase.initializeApp(config);
